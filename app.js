6d70000ac61a586504346204f66723528ea80457
Guido <gvilarino@gmail.com>
2014-09-24 00:38:30 -0300

    Refactor app.js and make event handling logic self-contained

--- app.js
+++ app.js
@@ -1,71 +1,7 @@
 var notifier = require('./source/notifier');
-var config = require('./config');
-var db = require('./source/db')(config.db);
-var transport = require('./source/transport');
-var logger = require('./source/utils/logger');
-var templates = require('./source/templates');
-var t = require('./source/translations').t;
 
-function formatName (user) {
-  return user.lastName ? user.firstName + ' ' + user.lastName : user.firstName
-}
-
-// initialize actions, resolvers and executors
-notifier
-    .receive('signup', function (event, actions, callback) {
-      logger.info('Received event ' + JSON.stringify(event));
-
-      actions.create('send-welcome', { user: event.user, validateUrl: event.validateUrl }, function (err) {
-        logger.info({message: 'Created "send-welcome" action for user ' + event.user });
-        callback && callback(err);
-      });
-
-    })
-
-    .resolve('send-welcome', function (action, actions, callback) {
-      logger.info('Resolving action ' + JSON.stringify(action));
-      db.user.findOne({ email: action.user }, function (err, user) {
-        if (err) return callback(err);
-        if (!user) return callback({message: 'user not found', email: action.email});
-
-        var data = {
-          email: user.email,
-          user: { name: formatName(user) },
-          validateUrl: action.validateUrl
-        }
-
-        logger.info('Received action ' + JSON.stringify(action) + ' with data ' + JSON.stringify(data));
-        actions.resolved(action, data, callback);
-      });
-    })
-
-    .execute('send-welcome', function (action, transport, callback) {
-        var vars = [
-          {name: 'USER_NAME', content: action.data.user.name},
-          {name: 'VALIDATE_MAIL_URL', content: action.data.validateUrl}
-        ];
-
-        // Add get content from jade template
-        templates.jade('welcome-email', vars, function (err, content) {
-
-          transport.mandrill('/messages/send', {
-              message: {
-                auto_html: null,
-                to: [{email: action.data.email}],
-                preserve_recipients: false,
-                from_email: config.transport.mandrill.from.email,
-                from_name: config.transport.mandrill.from.name,
-                subject: t('templates.welcome-email.subject'),
-                text: content,
-                html: content,
-                auto_text: true
-              }
-            }, function (err) {
-              callback && callback(err);
-            });
-
-        });
-});
+// Custom event definitions
+require('./source/events/signup')(notifier);
 
 // start the server
 notifier.start(9001);
---
+++ source/events/signup.js
@@ -0,0 +1,75 @@
+var config = require('../../config');
+var db = require('../db')(config.db);
+var transport = require('../transport');
+var logger = require('../utils/logger');
+var templates = require('../templates');
+var t = require('../translations').t;
+
+module.exports = function signup(notifier) {
+  if (!notifier || typeof notifier != 'object') throw new Error('Unable to initialize singup event - Undefined notifier');
+
+  // Receiver
+  notifier
+    .receive('signup', function (event, actions, callback) {
+      logger.info('Received event ' + JSON.stringify(event));
+
+      actions.create('send-welcome', { user: event.user, validateUrl: event.validateUrl }, function (err) {
+        logger.info({message: 'Created "send-welcome" action for user ' + event.user });
+        callback && callback(err);
+      });
+
+    })
+
+  // Resolver
+  notifier.resolve('send-welcome', function (action, actions, callback) {
+    logger.info('Resolving action ' + JSON.stringify(action));
+    db.user.findOne({ email: action.user }, function (err, user) {
+      if (err) return callback(err);
+      if (!user) return callback({message: 'user not found', email: action.email});
+
+      var data = {
+        email: user.email,
+        user: { name: formatName(user) },
+        validateUrl: action.validateUrl
+      }
+
+      logger.info('Received action ' + JSON.stringify(action) + ' with data ' + JSON.stringify(data));
+      actions.resolved(action, data, callback);
+    });
+  })
+
+  // Executor
+  .execute('send-welcome', function (action, transport, callback) {
+      var vars = [
+        {name: 'USER_NAME', content: action.data.user.name},
+        {name: 'VALIDATE_MAIL_URL', content: action.data.validateUrl}
+      ];
+
+      // Add get content from jade template
+      templates.jade('welcome-email', vars, function (err, content) {
+
+        transport.mandrill('/messages/send', {
+            message: {
+              auto_html: null,
+              to: [{email: action.data.email}],
+              preserve_recipients: false,
+              from_email: config.transport.mandrill.from.email,
+              from_name: config.transport.mandrill.from.name,
+              subject: t('templates.welcome-email.subject'),
+              text: content,
+              html: content,
+              auto_text: true
+            }
+          }, function (err) {
+            callback && callback(err);
+          });
+
+      });
+  });
+}
+
+
+
+function formatName (user) {
+  return user.lastName ? user.firstName + ' ' + user.lastName : user.firstName
+}
