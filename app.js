932b011d01e48be2300507053112c208ac5967aa
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2014-09-26 16:03:41 -0300

    Add reply-argument notification. Close #9

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var notifier = require('./source/notifier');
 // Custom event definitions
 require('./source/events/signup')(notifier);
 require('./source/events/forgot-password')(notifier);
+require('./source/events/reply-argument')(notifier);
 
 // start the server
 notifier.start(9001);
