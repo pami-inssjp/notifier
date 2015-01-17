f4282953d9596e3eb156fced1882c949cdd32bf9
David A. Lareo <dalareo@gmail.com>
2015-01-17 11:30:38 +0100

    Galician translation

--- source/translations/index.js
+++ source/translations/index.js
@@ -15,6 +15,7 @@ var ca = require('./lib/ca');
 var de = require('./lib/de');
 var fr = require('./lib/fr');
 var fi = require('./lib/fi');
+var gl = require('./lib/gl');
 var it = require('./lib/it');
 var nl = require('./lib/nl');
 var pt = require('./lib/pt');
