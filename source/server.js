f446490ce8daebad4cadda59bca0b46e2ca561f4
Guido <gvilarino@gmail.com>
2015-03-16 22:20:59 -0700

    Fix usage of heroku-ssl-redirect

--- source/server.js
+++ source/server.js
@@ -18,9 +18,9 @@ var cors = function (req, res, next) {
 };
 
 app.configure(function(){
-	app.use(sslRedirect()); // only works when `NODE_ENV` == 'production'
 	app.use(express.bodyParser());
 	app.use(express.cookieParser());
+	app.use(sslRedirect()); // only works when `NODE_ENV` == 'production'
 	app.use(cors);
 	app.use(express.methodOverride());
 	app.use(app.router);
